a6e296fd51656832f0ad41c4a3e232099a69c843
smileyhead <szijartoleventepal@gmail.com>
2025-10-06 10:44:19 +0200

    Add Hungarian (formal and informal) translation

---
+++ lang/hu-informal/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Hungarian (informal) language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author :     David Lorentsen <zyberdog@quakenet.org>
+ * 
+ * Translation credits:
+ * @author:     Szíjártó Levente Pál <szijartoleventepal@gmail.com>
+ */
+
+$lang['redirect_to']     = "Ez a szócikk elköltözött. Az új helye: %s.";
+$lang['redirected_from'] = "Átirányítva innen: %s.";
---
+++ lang/hu/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Hungarian (formal) language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author :     David Lorentsen <zyberdog@quakenet.org>
+ *
+ * Translation credits:
+ * @author:     Szíjártó Levente Pál <szijartoleventepal@gmail.com>
+ */
+
+$lang['show_note'] = 'Az átirányításról tájékoztató üzenet megjelenítése a céllapon';
+$lang['allow_external'] = 'Külső weblapokra történő átirányítás engedélyezése';
